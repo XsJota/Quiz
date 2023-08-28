20bce5863cb00b51e487d482cacee17ae64e0ec7
Jhonnatan <jhonnatan.jncp@gmail.com>
2023-08-28 18:01:30 -0300

    refactor: Improvement in organization

--- frontend/assets/js/quiz.js
+++ frontend/assets/js/quiz.js
@@ -1,10 +1,97 @@
-// Renderizando Div
+var inGame = false;
+var currentQuestion = 0;
+const quizQuestions = [
+  {
+    question: "Qual o maior planeta em nosso Sistema Solar?",
+    options: ["Marte", "Jupiter", "Vênus", "Saturno"],
+    answer: 1,
+  },
+  {
+    question: "Qual a capital da França?",
+    options: ["Paris", "Madrid", "London", "Roma"],
+    answer: 0,
+  },
+  {
+    question: "Quem foi Leonardo da Vinci?",
+    options: ["Ator", "Programador", "Roteirista", "Cientista"],
+    answer: 3,
+  },
+  {
+    question: "Qual o símbolo do Ouro na Tabela Periódica?",
+    options: ["Go", "Gd", "Au", "Ag"],
+    answer: 2,
+  },
+  {
+    question: "Qual o maior animal terrestre do mundo?",
+    options: ["Elefante", "Girafa", "Hipopótamo", "Rinoceronte"],
+    answer: 0,
+  },
+  {
+    question: "Qual a tradução da palvavra 'worse'? ",
+    options: ["O pior", "Ruim", "Bom", "Pior que"],
+    answer: 3,
+  }
+];
+playGame(inGame)
 
-$("body").append(`
-        <div class="flex justify-center items-center h-screen">
+const render = () => {
+  $("body").empty();
+
+  $("body").append(`
+      <div class="flex justify-center items-center h-screen">
+          <div class="container w-full max-w-xl ">
+              <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
+
+                  <div class="logo mr-5 max-w-xs max-w-xs">
+                    <img src="assets/img/quiz2.png" alt="Logo Quiz" srcset="">
+                  </div>
+              
+                  <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
+                      <div class="flex items-center justify-between mb-4">
+                        <div class="text-lg font-semibold">Questão 1 de 5</div>
+                      <div class="text-lg font-semibold">Score: 0</div>
+                  </div>
+                  <div class="mb-6">
+                     <h2 class="text-xl font-semibold">Qual é a capital da França?</h2>
+                  </div>
+                  <div class="mb-4">
+                      <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Paris</button>
+                  </div>
+                  <div class="mb-4">
+                      <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Berlin</button>
+                  </div>
+                  <div class="mb-4">
+                      <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Madrid</button>
+                  </div>
+                  <div class="mb-4">
+                      <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Rio de Janeiro</button>
+                  </div>
+
+                  <div class="h-4 relative w-full bg-gray-300 rounded">
+                      <div class="h-full bg-yellow-500 rounded" style="width: 10%;"></div>
+                  </div>
+
+                  <div class="mt-4">
+                      <button class="w-full bg-red-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
+                  </div>
+                  
+
+              </div>
+
+          </div>
+      </div>
+  </div>
+`);
+}; 
+
+function playGame(status){
+  inGame = status;
+  if(inGame) {
+    render();
+  }
+  $("body").append(`
+    <div class="flex justify-center items-center h-screen">
         <div class="container w-full max-w-xl ">
-
-
             <div class="px-32 pt-6 pb-8 mb-4 ">
 
                 <div class="logo mr-5 max-w-xs max-w-xs">
@@ -27,87 +114,60 @@ $("body").append(`
 
             </div>
         </div>
-        </div>
-    `);
-
-  // Evento de clique
+    </div>
+`);
+};
 
 $("#start").click(function () {
-    
-    $("body").empty()
-
-  $("body").append(`
-        <div class="flex justify-center items-center h-screen">
-        <div class="container w-full max-w-xl ">
-            <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
-
-                <div class="logo mr-5 max-w-xs max-w-xs">
-                    <img src="assets/img/quiz2.png" alt="Logo Quiz" srcset="">
-                </div>
-                
-                <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
-                    <div class="flex items-center justify-between mb-4">
-                        <div class="text-lg font-semibold">Questão 1 de 5</div>
-                        <div class="text-lg font-semibold">Score: 0</div>
-                    </div>
-                    <div class="mb-6">
-                        <h2 class="text-xl font-semibold">Qual é a capital da França</h2>
-                    </div>
-                    <div class="mb-4">
-                        <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Paris</button>
-                    </div>
-                    <div class="mb-4">
-                        <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Berlin</button>
-                    </div>
-                    <div class="mb-4">
-                        <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Madrid</button>
-                    </div>
-                    <div class="mb-4">
-                        <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">Rio de Janeiro</button>
-                    </div>
-
-                    <div class="h-4 relative w-full bg-gray-300 rounded">
-                        <div class="h-full bg-yellow-500 rounded" style="width: 10%;"></div>
-                    </div>
-
-                    <div class="mt-4">
-                        <button class="w-full bg-red-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
-                    </div>
-                    
-
-                </div>
-
-            </div>
-        </div>
-    </div>
-    `);
-    
+  inGame = true
+  playGame(inGame)
 });
 
-const quizQuestions = [
-  {
-    question: "Qual o maior planeta em nosso Sistema Solar?",
-    options: ["Marte", "Jupiter", "Vênus", "Saturno"],
-    answer: 1,
-  },
-  {
-    question: "Qual a capital da França?",
-    options: ["Paris", "Madrid", "London", "Rome"],
-    answer: 0,
-  },
-  {
-    question: "Quem foi Leonardo da Vint?",
-    options: ["Ator", "Programador", "Roteirista", "Engenheiro"],
-    answer: 3,
-  },
-  {
-    question: "Qual o símbolo do Ouro na Tabela Periódica?",
-    options: ["Go", "Gd", "Au", "Ag"],
-    answer: 2,
-  },
-  {
-    question: "Qual o maior animal terrestre do mundo?",
-    options: ["Elefante", "Girafa", "Hipopótamo", "Rinoceronte"],
-    answer: 1,
-  },
-];
+
+
+/* 
+const questionConteiner = document.getElementById('question-conteiner');
+const questionText = document.getElementById('question');
+const nextButton = document.getElementById('correct');
+
+var numberQuestionIndex = 0;
+
+// Limpar as respostas anteriores
+
+function showQuestion(quizQuestions){
+  questionText.textContent = quizQuestions.question;
+  // Limpar as respostas anteriores
+ while(questionConteiner.firstChild) {
+   questionContainer.removeChild(questionContainer.firstChild);
+ }
+ 
+ options.answer.forEach(answer => {
+  const button = document.createElement('button');
+  button.textContent = answer.options;
+  button.addEventListener('click', correctAnswer(answer.answer));
+  questionConteiner.appendChild(button);
+  });
+
+};
+
+function correctAnswer(isCorrect){
+  if(isCorrect){
+    const result = document.createElement('p');
+    result.textContent = 'Resposta correta!';
+    questionConteiner.appendChild(result);
+  } else {
+    const result = document.createElement('p');
+    result.textContent = 'Resposta incorreta!';
+    questionConteiner.appendChild(result);
+  }
+  numberQuestionIndex = numberQuestionIndex + 1;
+  let x = numberQuestionIndex < quizQuestions.length ? showQuestion(quizQuestions[numberQuestionIndex]) : questionText.textContent = 'Quiz concluído'
+};
+
+nextButton.addEventListener('click', () => {
+  showQuestion(quizQuestions[numberQuestionIndex])
+});
+
+showQuestion(quizQuestions[numberQuestionIndex]);
+*/
+
