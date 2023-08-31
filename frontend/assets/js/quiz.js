a0cc72dae2d82ddd080161b70d2532d2750aa671
Jhonnatan <jhonnatan.jncp@gmail.com>
2023-08-31 20:50:54 -0300

    feat: Restart button

--- frontend/assets/js/quiz.js
+++ frontend/assets/js/quiz.js
@@ -109,6 +109,36 @@ const submitButton = (event) => {
      `);
   }
 };
+const homeScreen = () => {
+  $("body").empty()
+  $("body").append(`
+    <div class="flex justify-center items-center h-screen">
+        <div class="container w-full max-w-xl ">
+            <div class="px-32 pt-6 pb-8 mb-4 ">
+
+                <div class="logo mr-5 max-w-xs max-w-xs">
+                    <img src="assets/img/quiz2.png" alt="Logo Quiz" srcset="">
+                </div>
+                
+                <div>
+                    <div class="p-2 text-center bg-yellow-500/100 rounded-md hover:bg-yellow-600">
+                        <button id='start' type="button" onclick="">Iniciar</button>
+                    </div>
+
+                    <div class="my-7 text-center p-2 bg-yellow-500/100 rounded-md hover:bg-yellow-600">
+                        <button type="button" onclick="">Recordes</button>
+                    </div>
+
+                    <div class="my-7 text-center p-2 bg-yellow-500/100 rounded-md hover:bg-yellow-600">
+                        <button type="button" onclick="">Encerrar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+`);
+}
 function playGame(status) {
   inGame = status;
   if (inGame) {
