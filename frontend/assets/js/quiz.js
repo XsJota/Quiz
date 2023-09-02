2c75e1dc03f0c0c212f2e202589ee232e7281ab1
Jhonnatan <jhonnatan.jncp@gmail.com>
2023-09-02 15:32:43 -0300

    commit: End Quiz

--- frontend/assets/js/quiz.js
+++ frontend/assets/js/quiz.js
@@ -1,12 +1,8 @@
+$(document).ready(function(){
 var inGame = false;
 var score = 0;
 var currentQuestion = 0;
 const quizQuestions = [
-  {
-    question: "Qual o maior planeta em nosso Sistema Solar?",
-    options: ["Marte", "Jupiter", "Vênus", "Saturno"],
-    answer: 1,
-  },
   {
     question: "Qual a capital da França?",
     options: ["Paris", "Madrid", "London", "Roma"],
@@ -28,10 +24,35 @@ const quizQuestions = [
     answer: 0,
   },
   {
+    question: "Quantos planetas existem no sistema solar?",
+    options: ["5", "7", "8", "9"],
+    answer: 2,
+  },
+  {
+    question: "Qual o maior planeta em nosso Sistema Solar?",
+    options: ["Marte", "Jupiter", "Vênus", "Saturno"],
+    answer: 1,
+  },
+  {
+    question: "Quem escreveu a obra 'Dom Quixote'?",
+    options: ["William Shakespeare", "Charles Dickens", "Miguel de Cervantes", "Friedrich Nietzsche"],
+    answer: 2,
+  },
+  {
     question: "Qual a tradução da palvavra 'worse'? ",
     options: ["O pior", "Ruim", "Bom", "Pior que"],
     answer: 3,
-  }
+  },
+  {
+    question: "Qual seleção nacional venceu a Copa do Mundo de 2002?",
+    options: ["Brasil", "Alemanha", "Argentina", "França"],
+    answer: 0
+  },
+  {
+    question: "Qual é a capital do Brasil?",
+    options: ["Rio de Janeiro", "São Paulo", "Brasília", "Salvador"],
+    answer: 2,
+  },
 ];
 
 playGame(inGame);
@@ -39,45 +60,52 @@ playGame(inGame);
 const loadQuestion = () => {
   $("body").empty();
   $("body").append(`
-      <div class="flex justify-center items-center h-screen">
-          <div class="container w-full max-w-xl ">
-              <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
-
-                  <div class="logo mr-5 max-w-xs max-w-xs">
-                    <img src="assets/img/quiz2.png" alt="Logo Quiz" srcset="">
-                  </div>
-              
-                  <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
-                      <div class="flex items-center justify-between mb-4">
-                        <div class="text-lg font-semibold">Questão ${(currentQuestion + 1)} de ${quizQuestions.length}</div>
-                      <div class="text-lg font-semibold">Score: ${score}</div>
-                  </div>
-                  <div class="mb-6">
-                     <h2 id='question' class="text-xl font-semibold">${quizQuestions[currentQuestion].question}</h2>
-                  </div>
+  <div class="flex justify-center items-center h-screen">
+  <div class="container w-full max-w-xl ">
+  <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
+  
+  <div class="logo mr-5 max-w-xs max-w-xs">
+  <img src="assets/img/quiz2.png" alt="Logo Quiz" srcset="">
+  </div>
+  
+  <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
+  <div class="flex items-center justify-between mb-4">
+  <div class="text-lg font-semibold">Questão ${(currentQuestion + 1)} de ${quizQuestions.length}</div>
+  <div class="text-lg font-semibold">Score: ${score}</div>
+  </div>
+  <div class="mb-6">
+  <h2 id='question' class="text-xl font-semibold">${quizQuestions[currentQuestion].question}</h2>
+  </div>
               </div>
           </div>
-      </div>
-  </div>
+          </div>
+          </div>
   `)
   for (i = 0; i < quizQuestions[currentQuestion].options.length; i++) {
     $(".bg-white").append(`
     <div class="mb-4">
-      <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">${quizQuestions[currentQuestion].options[i]}</button>
+    <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-yellow-600">${quizQuestions[currentQuestion].options[i]}</button>
     </div>
     `);
   }
-    $(".bg-white").append(`
-      <div class="h-4 relative w-full bg-gray-300 rounded">
-        <div class="h-full bg-yellow-500 rounded" style="width: ;">${(currentQuestion + 1) / quizQuestions.length * 100}</div>
-      </div>
-    `)
-    $(".bg-white").append(`
-      <div class="mt-4">
-        <button class="w-full bg-red-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
-      </div>
-    `)
+  $(".bg-white").append(`
+  <div class="h-4 relative w-full bg-gray-300 rounded">
+  <div class="h-full bg-yellow-500 rounded" style="width:${(currentQuestion + 1) / quizQuestions.length * 100}%;"></div>
+  </div>
+  `)
+  $(".bg-white").append(`
+  <div class="mt-4">
+  <button class="w-full bg-red-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
+  </div>
+  `)
   $(".mb-4").click(submitButton);
+  $(".mt-4").click(function() {
+    $("body").empty();
+    currentQuestion = 0;
+    score = 0;
+    loadQuestion();
+  });
+  $("h-full").click()
 };
 
 const submitButton = (event) => {
@@ -92,34 +120,48 @@ const submitButton = (event) => {
     loadQuestion();
     return;
   }else{
-  $("body").empty();
+    $("body").empty();
     $("body").append(`
-      <div class="flex justify-center items-center h-screen">
-        <div class="container w-full max-w-xl">
-          <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
-             <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
+    <div class="flex justify-center items-center h-screen">
+      <div class="container w-full max-w-xl">
+         <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
+            <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
               <div class="text-center">
                 <h2 class="text-xl font-semibold">Quiz Completed</h2>
                 <p>Your Score: ${score} out of ${quizQuestions.length}</p>
-                </div>
+              </div>
             </div>
-           </div>
-         </div>
-       </div>
-     `);
+          </div>
+        </div>
+    </div>
+    `);
   }
     $(".text-center").append(`
-       <div class="mt-4">
-         <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
-       </div>
+        <div class="mt-4">
+          <button class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-red-600">Reiniciar</button>
+        </div>
+    `)
+         
+    $(".text-center").append(`
+      <div class="mt-5">
+        <button id='end' class="w-full bg-yellow-400 text-white py-2 rounded-md hover:bg-red-600">Encerrar</button>
+      </div>
     `)
 
-  $(".mt-4").click(function() {
+    $("#end").click(function() {
+      $("body").empty();
+      inGame = false;
+      score = 0;
+      currentQuestion = 0;
+      playGame();
+    });
+
+    $(".mt-4").click(function() {
     $("body").empty();
-    currentQuestion = 0;
-    score = 0;
-    loadQuestion();
-  });
+      currentQuestion = 0;
+      score = 0;
+      loadQuestion();
+    });
 };
 function playGame(status) {
   inGame = status;
@@ -149,14 +191,16 @@ function playGame(status) {
         <button type="button" onclick="">Encerrar</button>
       </div>
     </div>
+  </div>
+  `);
 
-            </div>
-        </div>
-    </div>
-`);
+  $("#start").click(function () {
+    inGame = true;
+    playGame(inGame);
+  });
 }
-
 $("#start").click(function () {
   inGame = true;
   playGame(inGame);
+});
 });
