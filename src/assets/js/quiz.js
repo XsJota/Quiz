9389da33e3c9a667464c0a6ed3cb49603f9ad7f3
XsJota <jhonnatancosta10@.com>
2026-08-27 16:54:17 -0300

    feat: update quiz UI and enhance close confirmation dialog

--- src/assets/js/quiz.js
+++ src/assets/js/quiz.js
@@ -85,25 +85,25 @@ $(document).ready(function(){
     $("body").empty();
     $("body").append(`
       <div class="flex justify-center items-center h-screen">
-        <div class="container w-full max-w-xl ">
-          <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
-            <div class="logo mr-5 max-w-xs max-w-xs">
-                <img src="./frontend/assets/img/quiz.png" alt="Logo Quiz" style="max-width: 150px; max-height: 150px;">
+          <div class="container w-full max-w-xl ">
+            <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
+              <div class="logo mr-5 max-w-xs max-w-xs">
+                  <img src="./frontend/assets/img/quiz2.png" alt="Logo Quiz" style="max-width: 150px; max-height: 150px;">
+              </div>
+              <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
+                  <div class="flex items-center justify-between mb-4">
+                      <div class="text-lg font-semibold">Questão ${(currentQuestion + 1)} de ${quizQuestions.length}</div>
+                      <div class="text-lg font-semibold">Score: ${score}</div>
+                  </div>
+                  <div class="mb-6">
+                      <h2 id='question' class="text-xl font-semibold">${quizQuestions[currentQuestion].question}</h2>
+                  </div>
+              </div>
             </div>
-            <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
-                <div class="flex items-center justify-between mb-4">
-                    <div class="text-lg font-semibold">Questão ${(currentQuestion + 1)} de ${quizQuestions.length}</div>
-                    <div class="text-lg font-semibold">Score: ${score}</div>
-                </div>
-                <div class="mb-6">
-                    <h2 id='question' class="text-xl font-semibold">${quizQuestions[currentQuestion].question}</h2>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-</div>
-
     `)
+    
     for (i = 0; i < quizQuestions[currentQuestion].options.length; i++) {
       $(".bg-white").append(`
       <div class="mb-4">
@@ -237,8 +237,25 @@ $(document).ready(function(){
     });
 
     $("#close").click(function () {
-      const confirmation = confirm("Tem certeza que deseja fechar o jogo?");
-      confirmation && window.close();
+      $.confirm({
+        title: 'Tem certeza que deseja fechar o jogo?',
+        content: 'Esta ação não pode ser desfeita. E apenas para diversão, apagará seu recorde.',
+        useBootstrap: false,
+        boxWidth: '450px',
+        backgroundDismiss: false,
+        buttons: {
+          cancelar: function () {},
+          encerrar: {
+            text: 'Encerrar',
+            btnClass: 'btn-red',
+            keys: ['enter', 'shift'],
+            action: function(){
+              localStorage.removeItem("Pontuação");
+              window.close();
+            }
+          }
+        }
+      });
     });
   }
   playGame();
