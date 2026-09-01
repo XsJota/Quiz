afb99bc172c50166c3e2e055f23d611748c99512
XsJota <jhonnatancosta10@.com>
2026-09-01 15:46:45 -0300

    feat: add quiz questions data for the quiz functionality

---
+++ src/js/data/questions.js
@@ -0,0 +1,62 @@
+export const quizQuestions = [
+    {
+      id: 1,
+      question: "Qual a capital da França?",
+      options: ["Paris", "Madrid", "London", "Roma"],
+      answer: 0,
+    },
+    {
+      id: 2,
+      question: "Quem foi Leonardo da Vinci?",
+      options: ["Ator", "Programador", "Roteirista", "Cientista"],
+      answer: 3,
+    },
+    {
+      id: 3,
+      question: "Qual o símbolo do Ouro na Tabela Periódica?",
+      options: ["Go", "Gd", "Au", "Ag"],
+      answer: 2,
+    },
+    {
+      id: 4,
+      question: "Qual o maior animal terrestre do mundo?",
+      options: ["Elefante", "Girafa", "Hipopótamo", "Rinoceronte"],
+      answer: 0,
+    },
+    {
+      id: 5,
+      question: "Quantos planetas existem no sistema solar?",
+      options: ["5", "7", "8", "9"],
+      answer: 2,
+    },
+    {
+      id: 6,
+      question: "Qual o maior planeta em nosso Sistema Solar?",
+      options: ["Marte", "Jupiter", "Vênus", "Saturno"],
+      answer: 1,
+    },
+    {
+      id: 7,
+      question: "Quem escreveu a obra 'Dom Quixote'?",
+      options: ["William Shakespeare", "Charles Dickens", "Miguel de Cervantes", "Friedrich Nietzsche"],
+      answer: 2,
+    },
+    {
+      id: 8,
+      question: "Qual a tradução da palvavra 'worse'? ",
+      options: ["O pior", "Ruim", "Bom", "Pior que"],
+      answer: 3,
+    },
+    {
+      id: 9,
+      question: "Qual seleção nacional venceu a Copa do Mundo de 2002?",
+      options: ["Brasil", "Alemanha", "Argentina", "França"],
+      answer: 0
+    },
+    {
+      id: 10,
+      question: "Qual é a capital do Brasil?",
+      options: ["Rio de Janeiro", "São Paulo", "Brasília", "Salvador"],
+      answer: 2,
+    },
+];
