bf043626bde586f6728bc4c2ff1e459d54f57d33
XsJota <jhonnatancosta10@.com>
2026-09-02 17:19:45 -0300

    fix: update image source path in menu and question views

--- src/js/ui/menu-view.js
+++ src/js/ui/menu-view.js
@@ -4,7 +4,7 @@ export const renderMenu = ($app) => {
             <div class="container w-full max-w-xl ">
             <div class="px-32 pt-6 pb-8 mb-4 ">
                 <div class="logo mr-5 max-w-xs max-w-xs">
-                <img src="../../public/assets/images/quiz.png" alt="Logo Quiz" srcset="">
+                <img src="./public/assets/images/quiz.png" alt="Logo Quiz" srcset="">
                 </div>
                 <div>
                 <div data-action="start" class="p-2 text-center bg-yellow-500/100 rounded-md hover:bg-yellow-600">
--- src/js/ui/question-view.js
+++ src/js/ui/question-view.js
@@ -18,7 +18,7 @@ export const renderQuestion = ($app, state) => {
             <div class="container w-full max-w-xl ">
               <div class="p-12 pb-10 mb-10 flex flex-col items-center justify-center">
                 <div class="logo mr-5 max-w-xs max-w-xs">
-                    <img src="../../public/assets/images/quiz.png" alt="Logo Quiz" style="max-width: 150px; max-height: 150px;">
+                    <img src="./public/assets/images/quiz.png" alt="Logo Quiz" style="max-width: 150px; max-height: 150px;">
                 </div>
                 <div class="bg-white p-8 rounded shadow-md w-96 shadow-lg shadow-gray-500/50">
                     <div class="flex items-center justify-between mb-4">
